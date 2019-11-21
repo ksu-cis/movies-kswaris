54363cd1f6327466c5ee799001529641b9bcee4e
Sam Waris <swaris@ksu.edu>
2019-11-21 12:10:08 -0800

    updates

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var movieEntires = document.getElementsByClassName("movie-entry")
+var form = document.GetElementById("Search-and-filter-form");
+form.addEventListener('submit', function (event)){
+    event.preventDefault();
+    alert('searching...');
+    var term = document.getElementById("search").value;
+    if (term) {
+        for (i = 0; i < movieEntries.length; i++) {
+            entry = movieEntries[i];
+            console.log(entry, entry.datalist);
+            if (entry.dataset.title.includes(term.ToLowerCase())) {
+                entry.style.display = "block";
+            }
+            else
+                entry.style.display = "none";
+        }
+    }
+}
